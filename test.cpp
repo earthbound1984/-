8d7bfb6357f5245e23b592f0044ea147bbc72af4
earthbound1984 <91579482+earthbound1984@users.noreply.github.com>
2022-05-03 19:10:44 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <stdio.h>
+#include <math.h>
+#define N 6
+int main()
+{
+	int a, b, i, c[N],d,flag;
+	for (a = 0; a < 6; a++)
+		scanf_s("%d", &c[a]);
+	for (b = 1; b <= N - 1; b++)
+	{
+		flag = 0;
+		for (i = 0; i < N - b; i++)
+		{
+			if (c[i] > c[i + 1])
+			{
+				d = c[i];
+				c[i] = c[i + 1];
+				c[i + 1] = d;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			printf("%d", flag);
+	}
+		for (a = 0; a < 6; a++)
+		printf("%4d", c[a]);
+
+	
+}
+
+
+
